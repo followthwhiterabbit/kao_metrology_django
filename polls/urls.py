a7dd2fafb9dd2e0c47c686740fa0810c5d91e63c
Seyit Kaan Karaalioglu <kaankaraalioglu@yahoo.com>
2023-11-15 15:25:45 +0100

    website under construction

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"), 
+    path("poll/", views.index, name="poll"), 
     # ex: /polls/5
     path("<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
